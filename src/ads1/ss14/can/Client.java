be948e76592e4b1a5cd6f1c53d871e4917328a0b
mokocha <jozef.mesaros@gmail.com>
2014-04-17 17:58:26 +0200

    Initial commit

---
+++ src/ads1/ss14/can/Client.java
@@ -0,0 +1,158 @@
+package ads1.ss14.can;
+
+import ads1.ss14.can.exceptions.CANException;
+import ads1.ss14.can.exceptions.NoAdditionalStorageAvailable;
+import ads1.ss14.can.exceptions.NoSuchDocument;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Client implements ClientInterface, ClientCommandInterface{
+
+    private String uniqueID;
+    private int networkXSize;
+    private int getNetworkYSize;
+    private int maxNumberOfDocuments;
+    private HashMap<Integer,Document> library;
+    private Position position;
+    private Area area;
+    private ArrayList<Client> neighbourList;
+
+    /**
+	 * Constructs a new Client
+	 * @param uniqueID the ID of the Client in the CAN
+	 * @param networkXSize the size along the horizontal axis in the CAN
+	 * @param networkYSize the size along the vertical axis in the CAN
+	 */
+	public Client(String uniqueID, int networkXSize, int networkYSize) {
+        this.uniqueID = uniqueID;
+        this.networkXSize = networkXSize;
+        this.getNetworkYSize = networkYSize;
+	}
+
+	@Override
+	public String getUniqueID() {
+		return uniqueID;
+	}
+
+	@Override
+	public void setMaxNumberOfDocuments(int m) {
+        maxNumberOfDocuments = m;
+	}
+
+	@Override
+	public int getMaxNumberOfDocuments() {
+		return maxNumberOfDocuments;
+	}
+
+	@Override
+	public Document getDocument(String documentName) throws NoSuchDocument {
+        int key = calculateKeyHash(documentName);
+
+        if(library.containsKey(key))
+            return library.get(key);
+
+        throw new NoSuchDocument();
+	}
+
+	@Override
+	public void storeDocument(Document d, Position p) throws NoAdditionalStorageAvailable, CANException {
+        //TODO position provided only for checking?
+		if(library.size() != maxNumberOfDocuments) {
+            if(position.equals(p)) {
+                library.put(calculateKeyHash(d.getName()),d);
+            } else throw new CANException("wrong location");
+        } else throw new NoAdditionalStorageAvailable();
+	}
+
+    public int calculateKeyHash(String s) {
+        int value = 0;
+
+        for(int i = 1; i <= s.length();i++) {
+            char c = s.charAt(i);
+            value += Character.valueOf(c) * i;
+        }
+        return value;
+    }
+
+
+	@Override
+	public void deleteDocument(String documentName) throws NoSuchDocument {
+		if(library.containsKey(documentName))
+            library.remove(documentName);
+
+        throw new NoSuchDocument();
+	}
+
+	@Override
+	public Position getPosition() {
+		return position;
+	}
+
+	@Override
+	public Area getArea() {
+		return area;
+	}
+	
+	@Override
+	public void setArea(Area newArea) {
+		area = newArea;
+	}
+
+	@Override
+	public Iterable<ClientInterface> getNeighbours() {
+		//TODO Implement me!
+		return null;
+	}
+	
+	@Override
+	public void addNeighbour(ClientInterface newNeighbour){
+		//TODO Implement me!
+	}
+	
+	@Override
+	public void removeNeighbour(String clientID) {
+		//TODO Implement me!
+	}
+	
+	@Override
+	public ClientInterface searchForResponsibleClient(Position p) {
+		//TODO Implement me!
+		return null;
+	}
+
+	@Override
+	public ClientInterface joinNetwork(ClientInterface entryPoint, Position p) throws CANException {
+		//TODO Implement me!
+
+        position = p;
+		return null;
+	}
+
+	@Override
+	public Iterable<Pair<Document, Position>> removeUnmanagedDocuments() {
+		//TODO Implement me!
+		return null;
+	}
+		
+	@Override
+	public void adaptNeighbours(ClientInterface joiningClient) {
+		//TODO Implement me!
+	}
+
+	@Override
+	public void addDocumentToNetwork(Document d) throws CANException {
+		//TODO Implement me!
+	}
+
+	@Override
+	public void removeDocumentFromNetwork(String documentName) {
+		//TODO Implement me!
+	}
+
+	@Override
+	public Document searchForDocument(String documentName) throws CANException {
+		//TODO Implement me!
+		return null;
+	}
+}
